e941f5e7fa12d5211925425dd6e0ca1e23d8d79b
Braulio Ribeiro <braulio_holtz@hotmai.com>
2013-10-25 01:01:49 -0200

    some fixes and created a function read_message

--- main.cpp
+++ main.cpp
@@ -7,36 +7,55 @@ using namespace std;
 // Função para criar uma mensagem
 bool create_message(string message, string filename)
 {
-   ofstream arq;
-   arq.open("text.txt"); // fix: Colocar uma variavel para criação de várias mensagens
-   int i=0;
-   if (arq == NULL)  // Se houve erro na abertura
-   {
-       return false;
-   }
-   else
-   {
+    ofstream arq;
+    filename = "criptografia/"+filename+".txt";
+    arq.open(filename.c_str()); // fix: Colocar uma variavel para criação de várias mensagens
+    if (arq.is_open())  // Se houve erro na abertura
+    {
         arq << message; /* Grava a string, caractere a caractere */
         arq.close();
         return true;
-   }
+
+    }
+    else
+    {
+        return false;
+    }
+}
+
+// Função para ler a mensagem
+string read_message(string filename)
+{
+    ifstream arq_read;
+    string texto;
+    arq_read.open(filename.c_str());
+    if (arq_read.is_open())  // Se houve erro na abertura
+    {
+        while(!arq_read.eof())
+        {
+            getline(arq_read, texto);
+            cout << texto << "\n";
+        }
+    }
 }
 
 // Função principal
 int main()
 {
     string message = "", filename = "";
+    cout << "Digite o nome do arquivo:\n";
     getline(cin, filename);
+    cout << "Digite a sua mensagem:\n";
     getline(cin, message);
     // Abre um arquivo TEXTO para LEITURA
     if (create_message(message, filename))  // Se houve erro na abertura
     {
-        printf("Arquivo criado com exito\n");
+        cout << "Arquivo criado com exito\n";
 
     }
     else
     {
-        printf("Problemas na abertura do arquivo\n");
+        cout << "Problemas na abertura do arquivo\n";
     }
     return 0;
 }
