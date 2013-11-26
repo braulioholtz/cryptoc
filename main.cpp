554eb74dc478f5281fe42c1240bb9a01bd2cf2c6
Braulio Ribeiro <braulio_holtz@hotmail.com>
2013-11-26 01:53:06 -0200

    Convert text in ASCII code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <dirent.h>
+#include <sstream>
 #include <string>
 #include <fstream>
 
@@ -50,9 +51,37 @@ string read_message(string filename)
 
 }
 
-// Função para criptografar a mensagem
-string Encrypt(string filename)
+// Função para concatenar as variaveis string e int
+string concatenate(std::string const& name, int i)
 {
+    stringstream s;
+    s << name << i;
+    return s.str();
+}
+
+string convertToASCII(string letter)
+{
+    string result = "";
+    for (int i = 0; i < letter.length(); i++)
+    {
+        char x = letter.at(i);
+        // fix ASCII code :)
+        if (int(x)<10 || int(x)==0)
+            result = result + "00";
+        if (int(x)<100 && int(x)>9)
+            result = result + "0";
+        result = concatenate(result, int(x));
+    }
+    return result;
+}
+
+
+// Função para criptografar a mensagem
+string Encrypt(string message)
+{
+    string Result;
+    Result = convertToASCII(message);
+    return Result;
 }
 
 // Função para descriptografar a mensagem
@@ -61,7 +90,7 @@ string Decrypt(string filename)
 }
 
 // Checar chave
-int check(long int e, long int phi)
+int check(int e, long int phi)
 {
     int i=0, FLAG = 0;
     for(i=3;e%i==0 && phi%i==0;i+2)
@@ -69,16 +98,16 @@ int check(long int e, long int phi)
         FLAG = 1;
     }
     FLAG = 0;
+    return FLAG;
 }
 
 // Função principal
 int main()
 {
-    long int p1 = 0, p2 = 0; // Números primos
-    int phi = 2525465465; // variaveis responsaveis pela criptografia (em teste)
-    int menu = 0;
+    long int p1 = 7, p2 = 17; // Números primos
+    long int n = 0, phi = 0; // variaveis responsaveis pela criptografia (em teste)
+    int menu = 0, e=0, FLAG=0;
     string message = "", filename = "";
-
     do
     {
         system("cls");
@@ -104,10 +133,18 @@ int main()
             {
                 cout << "Problemas na abertura do arquivo\n";
             }
-            cout << "Texto criptografado";
+            cout << "Texto descriptografado\n";
             cout << read_message(filename);
-            cout << "Texto descriptografado";
-            cout << read_message(filename);
+            message = Encrypt(message);
+            if (create_message(message, filename))
+            {
+                cout << "\nTexto criptografado\n";
+                cout << read_message(filename);
+            }
+            else
+            {
+                cout << "Problemas na abertura do arquivo\n";
+            }
             system("pause");
         }
         else if (menu == 2)
@@ -125,12 +162,21 @@ int main()
             {
                 cout << read_message(filename);
                 cout << "\n\n";
+                cout << Encrypt(filename);
                 system("pause");
             }
 
         }
     } while (menu==1 || menu==2);
-    cin >> p1 >> p2;
-    cout << check(p1, phi); // apenas teste, começo criptografia
+    n = p1 * p2;
+    phi=(p1-1)*(p2-1);
+    cout << phi << "\n";
+    /*do
+    {
+        cin >> e; // ?
+        FLAG = check(e, phi); // apenas teste, começo criptografia
+        Encrypt(e);
+    }while(FLAG==1);
+    cout << FLAG;*/
     return 0;
 }
